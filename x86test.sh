245e3b106052decabbfb5353b0a418d070e8bd7f
sirpdboy <sirpdboy@qq.com>
2020-11-07 00:23:38 +0800

    ttyd

---
+++ x86test.sh
@@ -0,0 +1,24 @@
+rm -rf ./package/lean/luci-theme-argon
+rm -rf ./package/lean/trojan
+rm -rf ./package/lean/v2ray
+rm -rf ./package/lean/v2ray-plugin
+rm -rf ./package/lean/luci-app-netdata
+rm -rf ./package/lean/luci-theme-opentomcat
+rm -rf ./feeds/diy/autocore
+#rm -rf ./package/lean/autocore
+rm -rf ./package/lean/default-settings
+rm -rf feeds/packages/utils/ttyd
+rm -rf package/lean/luci-app-ttyd/root/etc/init.d/ttyd
+sed -i 's/网络存储/存储/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
+sed -i 's/Turbo ACC 网络加速/ACC网络加速/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
+sed -i 's/Turbo ACC 网络加速/ACC网络加速/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
+sed -i 's/解锁网易云灰色歌曲/解锁灰色歌曲/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
+sed -i 's/家庭云//g' package/lean/luci-app-familycloud/luasrc/controller/familycloud.lua
+sed -i '/filter_/d' ./package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell TZ=UTC-8 date +%Y%m%d)-ipv6-Mini/g' include/image.mk
+echo "DISTRIB_REVISION='S$(TZ=UTC-8 date +%Y.%m.%d) ipv6-Mini'" > ./package/base-files/files/etc/openwrt_release1
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/adguardhome ./package/new/adguardhome
+svn co https://github.com/siropboy/luci-app-vssr-plus/trunk/  package/luci-app-vssr-plus
+svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lienol
+./scripts/feeds update -i
