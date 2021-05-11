03e2cdbd6fce330f7bfc225419489187810c444e
sirpdboy <sirpdboy@qq.com>
2021-05-11 15:48:31 +0800

    ksmbd cn

--- luci-app-ksmbd/luasrc/controller/ksmbd.lua
+++ luci-app-ksmbd/luasrc/controller/ksmbd.lua
@@ -6,12 +6,10 @@ function index()
 	if not nixio.fs.access("/etc/config/ksmbd") then
 		return
 	end
-	
-	entry({"admin","nas"},firstchild(),"NAS",44).dependent=false
-
+    entry({"admin", "control"}, firstchild(), "Control", 90).dependent = false
 	local page
 
-	page = entry({"admin", "nas", "ksmbd"}, cbi("ksmbd"), _("Network Shares"))
+	page = entry({"admin", "nas", "ksmbd"}, cbi("ksmbd"), _("网络共享"))
 	page.dependent = true
 end
 
