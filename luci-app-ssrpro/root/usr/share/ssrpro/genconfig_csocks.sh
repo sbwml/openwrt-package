ba1064c66c6054145110f95b482239b4e4ce9c67
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2021-08-15 00:35:38 +0800

    up ssrpro

--- luci-app-ssrpro/root/usr/share/ssrpro/genconfig_csocks.sh
+++ luci-app-ssrpro/root/usr/share/ssrpro/genconfig_csocks.sh
@@ -4,10 +4,10 @@
 
 cat <<-EOF >$6
 {
-    "tls_host" :"$1",
-    "out_addr" :"$2",
-    "tls_ip" : "$3",
-    "port" : "$4",
-    "uuid" :"$5"
+    "tls_host":"$1",
+    "out_addr":"$2",
+    "tls_ip": "$3",
+    "port": "$4",
+    "uuid":"$5"
 }
 EOF
