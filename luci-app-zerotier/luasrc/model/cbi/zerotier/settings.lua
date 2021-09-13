01e404661142287134670109eaca385eca125379
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2021-09-13 21:24:49 +0800

    ze

--- luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
+++ luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
@@ -9,10 +9,9 @@ e=t:option(Flag,"enable",translate("Enable"))
 
 e=t:option(DynamicList,"join",translate('ZeroTier Network ID'))
 
-e=t:option(Flag,"nat",translate("Auto NAT Clients"))
-e.description=translate("Allow ZeroTier clients access your LAN network")
+e=t:option(Flag,"nat",translate("Auto NAT Clients"),translate("Allow ZeroTier clients access your LAN network"))
 
-e=t:option(DummyValue,"opennewwindow",translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Zerotier.com\" onclick=\"window.open('https://my.zerotier.com/network')\" />"))
-e.description=translate("Create or manage your ZeroTier network, and auth clients who could access")
+e=t:option(DummyValue,"opennewwindow",translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Zerotier.com\" onclick=\"window.open('https://my.zerotier.com/network')\" />"),
+translate("Create or manage your ZeroTier network, and auth clients who could access"))
 
 return a
